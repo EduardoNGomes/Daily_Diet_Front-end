fc1a77351312bc791990715654bb6e4d7c593914
Eduardo N Gomes <eduardo_n_gomes.dev@hotmail.com>
2023-06-15 17:54:32 -0300

    Feature:Navigation to Profile

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,17 +1,25 @@
 import logo from '../assets/Logo.svg'
-
+import { useNavigate } from 'react-router-dom'
 interface ProfileProps {
   avatarUrl: string
 }
+
 export const Profile = ({ avatarUrl }: ProfileProps) => {
+  const navigate = useNavigate()
+
+  const handleNavigateToUpdateProfile = () => {
+    navigate('/user/edit')
+  }
+
   return (
-    <div className="flex items-center justify-between">
+    <header className="flex items-center justify-between">
       <img src={logo} alt="" />
       <img
         src={avatarUrl}
         alt=""
         className="h-10 w-fit rounded-full border-2 border-gray-2 object-cover"
+        onClick={handleNavigateToUpdateProfile}
       />
-    </div>
+    </header>
   )
 }
