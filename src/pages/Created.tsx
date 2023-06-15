49d381bf53c596919517db0f777d8b2171674c94
Eduardo N Gomes <eduardo_n_gomes.dev@hotmail.com>
2023-06-15 18:58:59 -0300

    Feature:Navigation to Home

--- src/pages/Created.tsx
+++ src/pages/Created.tsx
@@ -1,18 +1,20 @@
 import imgOnDiet from '../assets/on-diet.png'
 import imgOffDiet from '../assets/off-diet.png'
 import { TemplatePageCreated } from '../components/TemplatePageCreated'
+import { useNavigate } from 'react-router-dom'
 
 export const Created = () => {
+  const navigate = useNavigate()
   const params = {
     isOnDiet: false,
   }
-  const handleChangePage = () => {
-    console.log('hi')
+  const handlerChangePageToHome = () => {
+    navigate('/')
   }
 
   return (
     <TemplatePageCreated
-      onClick={handleChangePage}
+      onClick={handlerChangePageToHome}
       onDiet={params.isOnDiet === true}
       imgSrc={params.isOnDiet === true ? imgOnDiet : imgOffDiet}
       title={params.isOnDiet === true ? 'Continue assim!' : 'Que pena!'}
