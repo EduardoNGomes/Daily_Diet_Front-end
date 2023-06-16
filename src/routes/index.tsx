980898a1fdef303e8a4a35229d752b4b5eff6cf7
Eduardo N Gomes <eduardo_n_gomes.dev@hotmail.com>
2023-06-16 10:31:00 -0300

    Feature:Config axios

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,9 +4,9 @@ import { AuthRouter, AppRouter } from './app'
 import { RouterProvider } from 'react-router-dom'
 
 export const Routes = () => {
-  const [cookies] = useCookies(['@daily-diet-token'])
+  const [cookies] = useCookies(['token'])
 
-  if (cookies['@daily-diet-token']) {
+  if (cookies.token) {
     return <RouterProvider router={AppRouter} />
   } else {
     return <RouterProvider router={AuthRouter} />
