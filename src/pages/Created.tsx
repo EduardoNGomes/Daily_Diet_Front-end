c9cadc160c7c333353a2edd9c2cb7ac2e453dfd0
Eduardo N Gomes <eduardo_n_gomes.dev@hotmail.com>
2023-06-18 16:30:49 -0300

    Fix: Page details

--- src/pages/Created.tsx
+++ src/pages/Created.tsx
@@ -1,10 +1,10 @@
 import imgOnDiet from '../assets/on-diet.png'
 import imgOffDiet from '../assets/off-diet.png'
 import { TemplatePageCreated } from '../components/TemplatePageCreated'
-import { useNavigate, useParams, useSearchParams } from 'react-router-dom'
+import { useNavigate, useSearchParams } from 'react-router-dom'
 
 export const Created = () => {
-  const [searchParams, setSearchParams] = useSearchParams()
+  const [searchParams] = useSearchParams()
   const navigate = useNavigate()
 
   const handlerChangePageToHome = () => {
