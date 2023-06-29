1292b4c2975a4a1b4d4b8034384194b53b4a8f40
Eduardo N Gomes <eduardo_n_gomes.dev@hotmail.com>
2023-06-29 13:41:02 -0300

    fix: profile components

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -22,7 +22,7 @@ export const Profile = ({ avatarUrl, name }: ProfileProps) => {
 
   return (
     <header className="flex items-center justify-between">
-      <img src={logo} alt="" className="cursor-pointer hover:brightness-110" />
+      <img src={logo} alt="" />
       <div className="flex items-center justify-center gap-2">
         <div className="flex flex-col items-end justify-end self-end text-sm">
           <p className="text-right font-bold text-gray-1">{name}</p>
@@ -34,7 +34,7 @@ export const Profile = ({ avatarUrl, name }: ProfileProps) => {
           <img
             src={`${api.defaults.baseURL}/users/avatar/${avatarUrl}`}
             alt=""
-            className="h-14 w-14 rounded-full border-2 border-gray-2 object-cover"
+            className="h-14 w-14 cursor-pointer rounded-full border-2 border-gray-2 object-cover hover:brightness-110"
             onClick={handleNavigateToUpdateProfile}
           />
         )}
